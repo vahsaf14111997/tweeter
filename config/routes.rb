286d467b187a0140fbe2229d428449f0e8b6d6e8
vahsaf14111997 <vahsaf14111997@gmail.com>
2021-11-11 18:58:37 +0530

    Adding Post resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
-  root 'home#index'
+  resources :posts
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
