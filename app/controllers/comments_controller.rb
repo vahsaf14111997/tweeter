4aace511db63df8d17f86000276c2a3e0a736184
vahsaf14111997 <vahsaf14111997@gmail.com>
2021-11-12 20:18:36 +0530

    Added comments sections

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    respond_to do |format|
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    format.html { redirect_to posts_path, notice: "Comment was successfully created." }
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:comment, :post_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
